0028b97bf1529001751193bd20f19c5e2e2c3ef1
DESKTOP-L3OV7JP\ohad <ohadokk@gmail.com>
2016-09-03 16:53:36 +0300

    on yad2 outo update of products (problem on pohn gap) abity to uploud imge to server in yad2 product create split of yad2 to yad2 and yad2 create destroy abily of cemra

---
+++ App_Code/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for User
+/// </summary>
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public User(int i, string n) { Name = n; Id = i; }
+}
--- App_Code/WebService.cs
+++ App_Code/WebService.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.Services;
-
+using System.Data;
 /// <summary>
 /// Summary description for WebService
 /// </summary>
@@ -29,13 +30,53 @@ public class WebService : System.Web.Services.WebService {
     public int SignUser(string FirstName, string LastName, string Email, string Password)
     {
 
-        string conStr = @"Data Source=DESKTOP-Q4MIT7O\SQLEXPRESS;Initial Catalog=TopShop;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+        string conStr = @"Data Source=DESKTOP-L3OV7JP\SQLEXPRESS;Initial Catalog=TopShop;Integrated Security=True";
         SqlConnection con = new SqlConnection(conStr);
         SqlCommand com = new SqlCommand("INSERT INTO Users (FirstName,LastName,Email,Password) VALUES('" + FirstName + "','" + LastName + "','" + Email + "','" + Password + "')", con);
         com.Connection.Open();
         int rows = com.ExecuteNonQuery();
         com.Connection.Close();
         return rows;
-    }    
-    
+    }
+
+
+    [WebMethod]
+
+    public string UploadFile()
+    {
+        string lreturn = "success";
+        try
+        {
+            HttpPostedFile file = HttpContext.Current.Request.Files["file"];
+            string saveFile = "C:\\Users\\ohad.DESKTOP-L3OV7JP\\Downloads\\" + file.FileName;
+            file.SaveAs(saveFile);
+        }
+        catch (Exception ex)
+        {
+            lreturn = "fail. " + ex.Message;
+        }
+        return lreturn;
+    }
+
+    [WebMethod]
+    public string GetAllUsers()
+    {
+        List<User> l = new List<global::User>();
+
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-L3OV7JP\SQLEXPRESS;Initial Catalog=UserDB;Integrated Security=True");
+        SqlCommand com = new SqlCommand("SELECT * FROM UsersTB ", con);
+        com.Connection.Open();
+        SqlDataReader reader = com.ExecuteReader();
+        while (reader.Read())
+        {
+            l.Add(new User((int)reader["Id"], reader[1].ToString()));
+        }
+        reader.Close();
+        com.Connection.Close();
+
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        return js.Serialize(l);
+    }
+
 }
